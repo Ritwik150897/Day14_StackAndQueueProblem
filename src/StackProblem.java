a965592acf7cb2f3068d619e897b58690a941b1e
Ritwik <deshrutu@gmail.com>
2022-06-14 10:01:39 +0530

    Stack Created

---
+++ src/StackProblem.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+
+public class StackProblem {
+    public static void main(String[] args) {
+
+        System.out.println("Stack Problem");
+        
+        Stack<Integer> stack = new Stack<>();
+
+
+    }
+}
