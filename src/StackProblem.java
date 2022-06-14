e610a4f2584d31c7cbe77f6d4b2705a671faad53
Ritwik <deshrutu@gmail.com>
2022-06-14 10:04:28 +0530

    Stack created, data pushed

--- src/StackProblem.java
+++ src/StackProblem.java
@@ -4,9 +4,13 @@ public class StackProblem {
     public static void main(String[] args) {
 
         System.out.println("Stack Problem");
-        
+
         Stack<Integer> stack = new Stack<>();
 
+        stack.push(70);
+        stack.push(30);
+        stack.push(56);
 
+        System.out.println("Stack:" + stack);
     }
 }
