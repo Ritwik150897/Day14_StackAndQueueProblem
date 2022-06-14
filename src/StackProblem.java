3947d1eed006e5ac879628bddb0152efba1975f1
Ritwik <deshrutu@gmail.com>
2022-06-14 10:10:51 +0530

    UC1 - Stack created, data pushed

--- src/StackProblem.java
+++ src/StackProblem.java
@@ -2,7 +2,6 @@ import java.util.Stack;
 
 public class StackProblem {
     public static void main(String[] args) {
-
         System.out.println("Stack Problem");
 
         Stack<Integer> stack = new Stack<>();
